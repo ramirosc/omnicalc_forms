162c5ec367816116a1d9b3a62f220b145cef433a
ramirosc <ramiro@chicagobooth.edu>
2015-10-27 18:42:31 -0500

    count and payment

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,38 @@
 class CalculationsController < ApplicationController
-  def loan_payment
 
+def word_count
+
+@text = params[:user_text]
+@word_count =@text.split.count
+
+
+render("word_count.html.erb")
+
+end
+
+
+
+def loan_payment_form
+
+render("loan_payment_form.html.erb")
+
+end
+
+def word_count_form
+
+render("word_count_form.html.erb")
+
+end
+
+
+
+  def loan_payment
+    @apr = params[:apr].to_f
+    @years = params[:years].to_f
+    @principal = params[:principal].to_f
+
+    rate= @apr / 1200
+    nper= @years *12
+    @monthly_payment = (rate*@principal)/(1-(1+rate)**-nper)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   get("/loan_payment/results", { :controller => "calculations", :action => "loan_payment" })
 
+  get("/loan_payment/new", { :controller => "calculations", :action => "loan_payment_form" })
+
+  get("word_count/results", {:controller => "calculations", :action => "word_count"})
+
+ get("word_count/new", {:controller => "calculations", :action => "word_count_form"})
+
+
 end
